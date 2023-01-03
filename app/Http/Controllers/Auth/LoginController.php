124ff58d5e72e156031f57cd950f3894cc02ae22
ilham akhyar <ilhamakhyar202@gmail.com>
2023-01-03 11:01:56 +0700

    modify login and add template chat

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function username()
+    {
+        return 'phone_number';
+    }
+
+    protected function credentials(Request $request)
+    {
+        return ['phone_number' => $request->get('phone_number'), 'password' => $request->get('password')];
     }
 }
